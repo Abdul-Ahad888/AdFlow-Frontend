f673a6f7b519a4d039b33387132de3efc81597f7
Abdul-Ahad888 <aaabdulahad888@gmail.com>
2026-04-04 13:50:55 +0500

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,25 @@ import Campaigns from "./components/Campaigns"
 import CreativeEngine from "./pages/CreativeEngine"
 import ProtectedRoute from "./components/ProtectedRoute";
 
+function getInitialRoute() {
+  const token = localStorage.getItem("token");
+  return token ? "/dashboard" : "/login";
+}
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        
+
         <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
         <Route path="/create-brief" element={<ProtectedRoute><CreativeBreifBuilder /></ProtectedRoute>} />
         <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
         <Route path="/clients" element={<ProtectedRoute><Client /></ProtectedRoute>} />
         <Route path="/campaigns" element={<ProtectedRoute><Campaigns /></ProtectedRoute>} />
         <Route path="/creative-engine" element={<ProtectedRoute><CreativeEngine /></ProtectedRoute>} />
-        {/* Redirect root to login */}
-        {/* <Route path="/" element={<Navigate to="/login" />} /> */}
+        {/* Redirect root to dashboard if logged in, otherwise login */}
+        <Route path="/" element={<Navigate to={getInitialRoute()} />} />
       </Routes>
     </BrowserRouter>
   );
